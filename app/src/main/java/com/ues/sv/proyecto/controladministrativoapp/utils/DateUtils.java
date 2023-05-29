048dd49f095f3276b41aeefa098e1697b2e11799
Cesar Josue Muria Gomez <58366298+Bateyss@users.noreply.github.com>
2023-05-29 03:31:49 -0600

    Add utilidades

--- app/src/main/java/com/ues/sv/proyecto/controladministrativoapp/utils/DateUtils.java
+++ app/src/main/java/com/ues/sv/proyecto/controladministrativoapp/utils/DateUtils.java
@@ -9,12 +9,24 @@ import java.util.Locale;
 
 public class DateUtils {
     public static final String FORMAT_YYYY_MM_DD = "yyyy-MM-dd";
+    public static final String FORMAT_DD_MM_YYYY = "dd-MM_YYYY";
+    public static final String FORMAT_YYYY_MM_DD_HH_MM_SS = "yyyy-MM-dd HH-mm-ss";
 
     public static Date getDateFromStringFormat(String stringDate, String format) {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format, Locale.US);
         Date d = null;
         try {
             d = simpleDateFormat.parse(stringDate);
+        } catch (Exception ignore) {
+        }
+        return d;
+    }
+
+    public static String formatDate(Date date, String format) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format, Locale.US);
+        String d = null;
+        try {
+            d = simpleDateFormat.format(date);
         } catch (Exception ignore) {
         }
         return d;
@@ -30,4 +42,5 @@ public class DateUtils {
         datePicker.init(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DATE), null);
         return datePicker;
     }
+
 }
