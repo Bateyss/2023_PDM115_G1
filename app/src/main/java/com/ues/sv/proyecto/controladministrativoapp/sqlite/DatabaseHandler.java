eb7d86fc6ffe9b6f1d18279c51b09f8ab2fd15a5
Cesar Josue Muria Gomez <58366298+Bateyss@users.noreply.github.com>
2023-05-29 03:31:18 -0600

    Update DatabaseHandler.java
    declare Daos

--- app/src/main/java/com/ues/sv/proyecto/controladministrativoapp/sqlite/DatabaseHandler.java
+++ app/src/main/java/com/ues/sv/proyecto/controladministrativoapp/sqlite/DatabaseHandler.java
@@ -6,12 +6,26 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+import com.ues.sv.proyecto.controladministrativoapp.dao.CicloDao;
+import com.ues.sv.proyecto.controladministrativoapp.dao.CoordinadorDao;
+import com.ues.sv.proyecto.controladministrativoapp.dao.CursoDao;
+import com.ues.sv.proyecto.controladministrativoapp.dao.DocenteDao;
+import com.ues.sv.proyecto.controladministrativoapp.dao.EvaluacionDao;
+import com.ues.sv.proyecto.controladministrativoapp.dao.MateriaDao;
 import com.ues.sv.proyecto.controladministrativoapp.dao.PersonaDao;
+import com.ues.sv.proyecto.controladministrativoapp.dao.TipoEvaluacionDao;
 import com.ues.sv.proyecto.controladministrativoapp.dao.UsuarioDao;
+import com.ues.sv.proyecto.controladministrativoapp.models.Ciclo;
+import com.ues.sv.proyecto.controladministrativoapp.models.Coordinador;
+import com.ues.sv.proyecto.controladministrativoapp.models.Curso;
+import com.ues.sv.proyecto.controladministrativoapp.models.Docente;
+import com.ues.sv.proyecto.controladministrativoapp.models.Evaluacion;
+import com.ues.sv.proyecto.controladministrativoapp.models.Materia;
 import com.ues.sv.proyecto.controladministrativoapp.models.Persona;
+import com.ues.sv.proyecto.controladministrativoapp.models.TipoEvaluacion;
 import com.ues.sv.proyecto.controladministrativoapp.models.Usuario;
 
-@Database(entities = {Persona.class, Usuario.class}, version = 1)
+@Database(entities = {Persona.class, Usuario.class, Ciclo.class, Coordinador.class, Curso.class, Docente.class, Evaluacion.class, Materia.class, TipoEvaluacion.class}, version = 1)
 public abstract class DatabaseHandler extends RoomDatabase {
 
     private static volatile DatabaseHandler INSTANCE;
@@ -31,5 +45,19 @@ public abstract class DatabaseHandler extends RoomDatabase {
     public abstract PersonaDao personaDao();
 
     public abstract UsuarioDao usuarioDao();
+
+    public abstract CicloDao cicloDao();
+
+    public abstract CoordinadorDao coordinadorDao();
+
+    public abstract CursoDao cursoDao();
+
+    public abstract DocenteDao docenteDao();
+
+    public abstract EvaluacionDao evaluacionDao();
+
+    public abstract MateriaDao materiaDao();
+
+    public abstract TipoEvaluacionDao tipoEvaluacionDao();
 
 }
