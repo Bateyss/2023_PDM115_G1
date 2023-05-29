14d8350fb40302fa11ffc1ffb58062fe7da12d22
Cesar Josue Muria Gomez <58366298+Bateyss@users.noreply.github.com>
2023-05-29 03:34:20 -0600

    add Crud Ciclo

---
+++ app/src/main/java/com/ues/sv/proyecto/controladministrativoapp/views/RegistrarCicloActivity.java
@@ -0,0 +1,121 @@
+package com.ues.sv.proyecto.controladministrativoapp.views;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputLayout;
+import com.ues.sv.proyecto.controladministrativoapp.R;
+import com.ues.sv.proyecto.controladministrativoapp.models.Ciclo;
+import com.ues.sv.proyecto.controladministrativoapp.service.CicloService;
+import com.ues.sv.proyecto.controladministrativoapp.service.interfaces.CallBackVoidInterface;
+import com.ues.sv.proyecto.controladministrativoapp.utils.ValidationUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegistrarCicloActivity extends AppCompatActivity {
+
+
+    private TextInputLayout layouNumero, layouAnio;
+    private MaterialButton btnGuardar;
+
+    private boolean esEditar = Boolean.FALSE;
+
+    private CicloService cicloService;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar_ciclo);
+
+        layouNumero = findViewById(R.id.input_layout_numero);
+        layouAnio = findViewById(R.id.input_layout_anio);
+        btnGuardar = findViewById(R.id.btn_guardar);
+
+        cicloService = new CicloService(getApplicationContext());
+
+        btnGuardar.setOnClickListener(v -> {
+            if (validarDatos()) guardarRegistro();
+        });
+
+        cargarDatos();
+    }
+
+    private boolean validarDatos() {
+        Map<String, TextInputLayout> map = new HashMap<>();
+        map.put("numeroCiclo", layouNumero);
+        map.put("numeroAnio", layouAnio);
+        return ValidationUtils.validate(Ciclo.class, map);
+    }
+
+    private void guardarRegistro() {
+        try {
+            Ciclo ciclo = new Ciclo();
+            ciclo.setNumeroCiclo(layouNumero.getEditText().getText().toString());
+            ciclo.setNumeroAnio(layouAnio.getEditText().getText().toString());
+
+            try {
+                if (esEditar) {
+                    cicloService.editarEntidad(ciclo, new CallBackVoidInterface() {
+                        @Override
+                        public void onCallBack() {
+                            Toast.makeText(getBaseContext(), "almacenado", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getBaseContext(), VerCiclosActivity.class);
+                            startActivity(intent);
+                        }
+
+                        @Override
+                        public void onThrow(Throwable throwable) {
+
+                        }
+                    });
+                } else {
+                    cicloService.registrarEntidad(ciclo, new CallBackVoidInterface() {
+                        @Override
+                        public void onCallBack() {
+                            Toast.makeText(getBaseContext(), "almacenado", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getBaseContext(), VerCiclosActivity.class);
+                            startActivity(intent);
+                        }
+
+                        @Override
+                        public void onThrow(Throwable throwable) {
+
+                        }
+                    });
+                }
+
+            } catch (Exception ex) {
+                Log.e("GUARDAR_DATOS", ex.getMessage(), ex.getCause());
+                Toast.makeText(this, "Errorcillo", Toast.LENGTH_SHORT).show();
+            }
+
+        } catch (Exception e) {
+            Log.e("CARGAR_DATOS", e.getMessage(), e.getCause());
+        }
+    }
+
+    private void cargarDatos() {
+        try {
+            Bundle bundle = getIntent().getExtras();
+            Ciclo ciclo = new Ciclo();
+            ciclo.setIdCiclo(bundle.getLong("IdCiclo", 0L));
+            ciclo.setNumeroCiclo(bundle.getString("NumeroCiclo", null));
+            ciclo.setNumeroAnio(bundle.getString("NumeroAnio", null));
+
+            if (ciclo.getNumeroCiclo() != null && ciclo.getNumeroAnio() != null) {
+                layouNumero.getEditText().setText(ciclo.getNumeroCiclo());
+                layouAnio.getEditText().setText(ciclo.getNumeroAnio());
+                esEditar = Boolean.TRUE;
+            }
+
+        } catch (Exception e) {
+            Log.e("CARGAR_DATOS", e.getMessage(), e.getCause());
+        }
+    }
+}
---
+++ app/src/main/java/com/ues/sv/proyecto/controladministrativoapp/views/VerCiclosActivity.java
@@ -0,0 +1,107 @@
+package com.ues.sv.proyecto.controladministrativoapp.views;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textview.MaterialTextView;
+import com.ues.sv.proyecto.controladministrativoapp.R;
+import com.ues.sv.proyecto.controladministrativoapp.models.Ciclo;
+import com.ues.sv.proyecto.controladministrativoapp.service.CicloService;
+import com.ues.sv.proyecto.controladministrativoapp.service.interfaces.CallBackDisposableInterface;
+import com.ues.sv.proyecto.controladministrativoapp.service.interfaces.CallBackVoidInterface;
+import com.ues.sv.proyecto.controladministrativoapp.utils.adapters.OnlyTxtInterface;
+import com.ues.sv.proyecto.controladministrativoapp.utils.adapters.OnlyTxtRecyclerAdapter;
+
+import java.util.List;
+
+public class VerCiclosActivity extends AppCompatActivity {
+
+    private MaterialButton btnCrear, btnEditar, btnEliminar;
+    private RecyclerView recyclerView;
+    private Ciclo cicloSelected = null;
+    private CicloService cicloService;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ver_ciclos);
+
+        btnCrear = findViewById(R.id.btn_crear);
+        btnEditar = findViewById(R.id.btn_editar);
+        btnEliminar = findViewById(R.id.btn_eliminar);
+        recyclerView = findViewById(R.id.recyclerList);
+
+        cicloService = new CicloService(getApplicationContext());
+
+        btnCrear.setOnClickListener(v -> {
+            //Intent intent = new Intent(getBaseContext(), RegistrarAlumnoActivity.class);
+            //startActivity(intent);
+        });
+
+        cargarRecyclerList();
+    }
+
+    private void cargarRecyclerList() {
+        cicloService.obtenerListaEntidad(new CallBackDisposableInterface<List<Ciclo>>() {
+            @Override
+            public void onCallBack(List<Ciclo> ciclos) {
+                OnlyTxtRecyclerAdapter<Ciclo> recyclerAdapter = new OnlyTxtRecyclerAdapter<Ciclo>(ciclos, getBaseContext(), new OnlyTxtInterface<Ciclo>() {
+                    @Override
+                    public void imprimirdatos(MaterialTextView textView, Ciclo ciclo) {
+                        String txt = ciclo.getNumeroCiclo() + " - " + ciclo.getNumeroAnio();
+                        textView.setText(txt);
+                    }
+
+                    @Override
+                    public void onItemClick(ConstraintLayout constraint, Ciclo ciclo, int position) {
+                        cicloSelected = ciclo;
+                        if (cicloSelected != null) {
+                            btnEliminar.setEnabled(Boolean.TRUE);
+                            btnEditar.setEnabled(Boolean.TRUE);
+
+                            btnEditar.setOnClickListener(v -> {
+                                btnEliminar.setEnabled(Boolean.FALSE);
+                                /*btnEditar.setEnabled(Boolean.FALSE);
+                                Intent intent = new Intent(getBaseContext(), RegistrarAlumnoActivity.class);
+                                intent.putExtra("Carnet", alumno.getCarnet());
+                                intent.putExtra("Nombre", alumno.getNombre());
+                                intent.putExtra("Apellido", alumno.getApellido());
+                                intent.putExtra("MatGanadas", alumno.getMatGanadas());
+                                intent.putExtra("Sexo", alumno.getSexo());
+                                startActivity(intent);*/
+                            });
+
+                            btnEliminar.setOnClickListener(v -> {
+                                cicloService.eliminarEntidad(ciclo, new CallBackVoidInterface() {
+                                    @Override
+                                    public void onCallBack() {
+                                        btnEliminar.setEnabled(Boolean.FALSE);
+                                        btnEditar.setEnabled(Boolean.FALSE);
+                                        cargarRecyclerList();
+                                    }
+
+                                    @Override
+                                    public void onThrow(Throwable throwable) {
+
+                                    }
+                                });
+                            });
+                        }
+                    }
+                });
+                recyclerView.setLayoutManager(new GridLayoutManager(getBaseContext(), 1));
+                recyclerView.setAdapter(recyclerAdapter);
+            }
+
+            @Override
+            public void onThrow(Throwable throwable) {
+
+            }
+        });
+    }
+}
