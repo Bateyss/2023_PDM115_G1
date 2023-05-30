355c91d44df56308b313bb574249e5582d61704a
Cesar Josue Muria Gomez <58366298+Bateyss@users.noreply.github.com>
2023-05-29 18:23:44 -0600

    add Crud Docentes

---
+++ app/src/main/java/com/ues/sv/proyecto/controladministrativoapp/views/RegistrarDocenteActivity.java
@@ -0,0 +1,190 @@
+package com.ues.sv.proyecto.controladministrativoapp.views;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.textfield.MaterialAutoCompleteTextView;
+import com.google.android.material.textfield.TextInputLayout;
+import com.ues.sv.proyecto.controladministrativoapp.R;
+import com.ues.sv.proyecto.controladministrativoapp.models.Docente;
+import com.ues.sv.proyecto.controladministrativoapp.models.Persona;
+import com.ues.sv.proyecto.controladministrativoapp.service.DocenteService;
+import com.ues.sv.proyecto.controladministrativoapp.service.PersonaService;
+import com.ues.sv.proyecto.controladministrativoapp.service.interfaces.CallBackDisposableInterface;
+import com.ues.sv.proyecto.controladministrativoapp.service.interfaces.CallBackVoidInterface;
+import com.ues.sv.proyecto.controladministrativoapp.utils.DateUtils;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class RegistrarDocenteActivity extends AppCompatActivity {
+
+    private TextInputLayout layouPersona, layouFecha;
+    private MaterialButton btnGuardar;
+    private boolean esEditar = Boolean.FALSE;
+
+    private DocenteService docenteService;
+    private PersonaService personaService;
+
+    private Docente docenteData = new Docente();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar_docente);
+
+        layouPersona = findViewById(R.id.input_layout_persona);
+        layouFecha = findViewById(R.id.input_layout_fecha);
+        btnGuardar = findViewById(R.id.btn_guardar);
+
+        personaService = new PersonaService(getApplicationContext());
+        docenteService = new DocenteService(getApplicationContext());
+
+        MaterialDatePicker<Long> datePicker = MaterialDatePicker.Builder.datePicker().setTitleText("Seleccionar Fecha").build();
+
+        btnGuardar.setOnClickListener(v -> {
+            if (validarDatos()) guardarRegistro();
+        });
+
+        layouFecha.getEditText().setOnClickListener(v -> {
+            datePicker.show(getSupportFragmentManager(), "tag");
+            datePicker.addOnPositiveButtonClickListener(selection -> {
+                Long timeMilis = (long) selection;
+                Date dateSelected = new Date(timeMilis);
+                String dateTxt = DateUtils.formatDate(dateSelected, DateUtils.FORMAT_DD_MM_YYYY);
+                layouFecha.getEditText().setText(dateTxt);
+            });
+        });
+
+        cargarDatos();
+    }
+
+    private boolean validarDatos() {
+        boolean valid = true;
+        if (docenteData.getFechaIngreso() == null) {
+            layouFecha.setError("Seleccione Fecha de Ingreso");
+            valid = false;
+        }
+        if (docenteData.getPersona() == null) {
+            layouPersona.setError("Seleccione Persona");
+            valid = false;
+        }
+        return valid;
+    }
+
+    private void guardarRegistro() {
+        try {
+
+            try {
+                if (esEditar) {
+                    docenteService.editarEntidad(docenteData, new CallBackVoidInterface() {
+                        @Override
+                        public void onCallBack() {
+                            Toast.makeText(getBaseContext(), "almacenado", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getBaseContext(), VerDocentesActivity.class);
+                            startActivity(intent);
+                        }
+
+                        @Override
+                        public void onThrow(Throwable throwable) {
+
+                        }
+                    });
+                } else {
+                    docenteService.registrarEntidad(docenteData, new CallBackVoidInterface() {
+                        @Override
+                        public void onCallBack() {
+                            Toast.makeText(getBaseContext(), "almacenado", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getBaseContext(), VerDocentesActivity.class);
+                            startActivity(intent);
+                        }
+
+                        @Override
+                        public void onThrow(Throwable throwable) {
+
+                        }
+                    });
+                }
+
+            } catch (Exception ex) {
+                Log.e("GUARDAR_DATOS", ex.getMessage(), ex.getCause());
+                Toast.makeText(this, "Errorcillo", Toast.LENGTH_SHORT).show();
+            }
+
+        } catch (Exception e) {
+            Log.e("CARGAR_DATOS", e.getMessage(), e.getCause());
+        }
+    }
+
+    private void cargarDatos() {
+        try {
+            Bundle bundle = getIntent().getExtras();
+
+            docenteData.setIdDocente(bundle.getLong("IdDocente", 0L));
+            personaService.buscarPorId(bundle.getLong("IdPersona", 1L), new CallBackDisposableInterface<Persona>() {
+                @Override
+                public void onCallBack(Persona persona) {
+                    docenteData.setPersona(persona);
+                }
+
+                @Override
+                public void onThrow(Throwable throwable) {
+
+                }
+            });
+            Date d = DateUtils.getDateFromStringFormat(bundle.getString("FechaIngreso"), DateUtils.FORMAT_YYYY_MM_DD);
+            if (d != null) {
+                docenteData.setFechaIngreso(d);
+                String dateTxt = DateUtils.formatDate(d, DateUtils.FORMAT_DD_MM_YYYY);
+                layouFecha.getEditText().setText(dateTxt);
+            }
+
+            if (docenteData.getIdDocente() > 0 && docenteData.getFechaIngreso() != null) {
+                esEditar = Boolean.TRUE;
+            }
+
+        } catch (Exception e) {
+            Log.e("CARGAR_DATOS", e.getMessage(), e.getCause());
+        }
+        personaService.obtenerListaEntidad(new CallBackDisposableInterface<List<Persona>>() {
+            @Override
+            public void onCallBack(List<Persona> personas) {
+                List<String> personasStrings = new ArrayList<>();
+                personas.forEach(per -> personasStrings.add(per.getNombre() + " " + per.getApellido()));
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), R.layout.holder_item_only_text, R.id.only_text_txt_view, personasStrings);
+                MaterialAutoCompleteTextView autoCompleteTextView = (MaterialAutoCompleteTextView) layouPersona.getEditText();
+                if (autoCompleteTextView != null) {
+                    autoCompleteTextView.setAdapter(adapter);
+                    autoCompleteTextView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                            docenteData.setPersona(personas.get(position));
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+
+                        }
+                    });
+                    if (docenteData.getPersona() != null)
+                        autoCompleteTextView.setSelection(personas.indexOf(docenteData.getPersona()));
+                }
+            }
+
+            @Override
+            public void onThrow(Throwable throwable) {
+
+            }
+        });
+    }
+}
