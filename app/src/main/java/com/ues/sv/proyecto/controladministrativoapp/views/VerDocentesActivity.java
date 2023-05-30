2a9f1824962e6f711700b96caab3996e6ad79034
Cesar Josue Muria Gomez <58366298+Bateyss@users.noreply.github.com>
2023-05-29 18:24:08 -0600

    add crud docentes

---
+++ app/src/main/java/com/ues/sv/proyecto/controladministrativoapp/views/VerDocentesActivity.java
@@ -0,0 +1,106 @@
+package com.ues.sv.proyecto.controladministrativoapp.views;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textview.MaterialTextView;
+import com.ues.sv.proyecto.controladministrativoapp.R;
+import com.ues.sv.proyecto.controladministrativoapp.models.Docente;
+import com.ues.sv.proyecto.controladministrativoapp.service.DocenteService;
+import com.ues.sv.proyecto.controladministrativoapp.service.interfaces.CallBackDisposableInterface;
+import com.ues.sv.proyecto.controladministrativoapp.service.interfaces.CallBackVoidInterface;
+import com.ues.sv.proyecto.controladministrativoapp.utils.adapters.OnlyTxtInterface;
+import com.ues.sv.proyecto.controladministrativoapp.utils.adapters.OnlyTxtRecyclerAdapter;
+
+import java.util.List;
+
+public class VerDocentesActivity extends AppCompatActivity {
+
+    private MaterialButton btnCrear, btnEditar, btnEliminar;
+    private RecyclerView recyclerView;
+    private Docente docenteSelected = null;
+    private DocenteService docenteService;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ver_docentes);
+
+        btnCrear = findViewById(R.id.btn_crear);
+        btnEditar = findViewById(R.id.btn_editar);
+        btnEliminar = findViewById(R.id.btn_eliminar);
+        recyclerView = findViewById(R.id.recyclerList);
+
+        docenteService = new DocenteService(getApplicationContext());
+
+        btnCrear.setOnClickListener(v -> {
+            Intent intent = new Intent(getBaseContext(), RegistrarDocenteActivity.class);
+            startActivity(intent);
+        });
+
+        cargarRecyclerList();
+    }
+
+    private void cargarRecyclerList() {
+        docenteService.obtenerListaEntidad(new CallBackDisposableInterface<List<Docente>>() {
+            @Override
+            public void onCallBack(List<Docente> docentes) {
+                OnlyTxtRecyclerAdapter<Docente> recyclerAdapter = new OnlyTxtRecyclerAdapter<Docente>(docentes, getBaseContext(), new OnlyTxtInterface<Docente>() {
+                    @Override
+                    public void imprimirdatos(MaterialTextView textView, Docente docente) {
+                        String txt = docente.getPersona().getNombre() + " - " + docente.getPersona().getApellido();
+                        textView.setText(txt);
+                    }
+
+                    @Override
+                    public void onItemClick(ConstraintLayout constraint, Docente docente, int position) {
+                        docenteSelected = docente;
+                        if (docenteSelected != null) {
+                            btnEliminar.setEnabled(Boolean.TRUE);
+                            btnEditar.setEnabled(Boolean.TRUE);
+
+                            btnEditar.setOnClickListener(v -> {
+                                btnEliminar.setEnabled(Boolean.FALSE);
+                                btnEditar.setEnabled(Boolean.FALSE);
+                                Intent intent = new Intent(getBaseContext(), RegistrarDocenteActivity.class);
+                                intent.putExtra("IdDocente", docente.getIdDocente());
+                                intent.putExtra("FechaIngreso", docente.getFechaIngreso());
+                                intent.putExtra("IdPersona", docente.getPersona().getIdPersona());
+                                startActivity(intent);
+                            });
+
+                            btnEliminar.setOnClickListener(v -> {
+                                docenteService.eliminarEntidad(docente, new CallBackVoidInterface() {
+                                    @Override
+                                    public void onCallBack() {
+                                        btnEliminar.setEnabled(Boolean.FALSE);
+                                        btnEditar.setEnabled(Boolean.FALSE);
+                                        cargarRecyclerList();
+                                    }
+
+                                    @Override
+                                    public void onThrow(Throwable throwable) {
+
+                                    }
+                                });
+                            });
+                        }
+                    }
+                });
+                recyclerView.setLayoutManager(new GridLayoutManager(getBaseContext(), 1));
+                recyclerView.setAdapter(recyclerAdapter);
+            }
+
+            @Override
+            public void onThrow(Throwable throwable) {
+
+            }
+        });
+    }
+}
