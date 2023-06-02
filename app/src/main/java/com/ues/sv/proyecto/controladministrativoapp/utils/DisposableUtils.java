afe0a0d697fb7536d23eec008d123b14983fb2f7
Cesar Josue Muria Gomez <58366298+Bateyss@users.noreply.github.com>
2023-06-02 02:56:55 -0600

    Update DisposableUtils.java

--- app/src/main/java/com/ues/sv/proyecto/controladministrativoapp/utils/DisposableUtils.java
+++ app/src/main/java/com/ues/sv/proyecto/controladministrativoapp/utils/DisposableUtils.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.core.Completable;
 import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.core.Single;
 import io.reactivex.rxjava3.disposables.CompositeDisposable;
 import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
@@ -14,26 +15,34 @@ public class DisposableUtils {
     private static final CompositeDisposable mDisposable = new CompositeDisposable();
 
     public static void addComposite(CompositeCompletableCallback compositeInterface) {
-        mDisposable.add(compositeInterface.completableAction()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(compositeInterface::onCallback
-                        , throwable -> {
+        mDisposable.add(
+                compositeInterface.completableAction()
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(compositeInterface::onCallback, throwable -> {
                             Log.e("ADD_COMPLETABLE_COMPOSITE", "Error ejecutar completable", throwable);
                             compositeInterface.onThrow(throwable);
-                        })
-        );
+                        }));
+    }
+
+    public static void addComposite(CompositeSingleCallbac compositeInterface) {
+        mDisposable.add(compositeInterface.completableCallBack(
+                compositeInterface.singleAction()
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .doOnError(throwable ->
+                                Log.e("ADD_FLOWABLEs_COMPOSITE", "Error ejecutar flowable"
+                                        , throwable))));
     }
 
     public static void addComposite(CompositeFlowableCallback compositeInterface) {
-        mDisposable.add(
-                compositeInterface.completableCallBack(
-                        compositeInterface.flowableAction()
-                                .subscribeOn(Schedulers.io())
-                                .observeOn(AndroidSchedulers.mainThread())
-                                .doOnError(throwable -> Log.e("ADD_FLOWABLEs_COMPOSITE", "Error ejecutar flowable", throwable))
-                )
-        );
+        mDisposable.add(compositeInterface.completableCallBack(
+                compositeInterface.flowableAction()
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .doOnError(throwable ->
+                                Log.e("ADD_FLOWABLEs_COMPOSITE", "Error ejecutar flowable"
+                                        , throwable))));
     }
 
 
@@ -44,6 +53,14 @@ public class DisposableUtils {
         void onCallback();
 
         void onThrow(Throwable throwable);
+
+    }
+
+    public interface CompositeSingleCallbac {
+
+        Single<?> singleAction();
+
+        Disposable completableCallBack(Single<?> applySubscribe);
 
     }
 
